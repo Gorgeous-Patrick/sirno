08a3e8a1a34703edbf50d7550372f114a2fa641d
Patrick Li <baichuanli@yahoo.com>
2026-05-16 13:07:16 -0400

    repo: add nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "sirno";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "sirno";
+          version = cargoToml.package.version;
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.rust-analyzer
+          ];
+        };
+      });
+}
